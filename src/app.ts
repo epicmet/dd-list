5f9103e3f2d903cbd192a04a4a2a0aec2a33e71d
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-10-13 16:34:56 +0330

    injected template to root element

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-console.log("hello there");
+class ProjectInput {
+  templateEl: HTMLTemplateElement;
+  rootEl: HTMLDivElement;
+  formEl: HTMLFormElement;
+
+  constructor() {
+    this.templateEl = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.rootEl = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(this.templateEl.content, true);
+    this.formEl = importedNode.firstElementChild as HTMLFormElement;
+    this.formEl.id = "user-input";
+
+    this.rootEl.insertAdjacentElement("afterbegin", this.formEl);
+  }
+}
+
+const projInput = new ProjectInput();
