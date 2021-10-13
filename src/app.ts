98d1041dd97dc3eadb016c5c85ab050e9468fc11
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-10-13 16:57:01 +0330

    add event listener for submiting the form with utilizing SyncBind decorator

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,25 @@
+// Decorators
+function SyncBinding(
+  _1: any,
+  _2: string,
+  descriptor: PropertyDescriptor
+): PropertyDescriptor {
+  return {
+    configurable: true,
+    enumerable: false,
+    get() {
+      return descriptor.value.bind(this);
+    },
+  };
+}
+
 class ProjectInput {
   templateEl: HTMLTemplateElement;
   rootEl: HTMLDivElement;
   formEl: HTMLFormElement;
+  titleInputEl: HTMLInputElement;
+  descriptionInputEl: HTMLInputElement;
+  peopleInputEl: HTMLInputElement;
 
   constructor() {
     this.templateEl = document.getElementById(
@@ -13,7 +31,25 @@ class ProjectInput {
     this.formEl = importedNode.firstElementChild as HTMLFormElement;
     this.formEl.id = "user-input";
 
+    this.titleInputEl = this.formEl.querySelector("#title") as HTMLInputElement;
+    this.descriptionInputEl = this.formEl.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputEl = this.formEl.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
+    // Submit Listener
+    this.formEl.addEventListener("submit", this.submitHandler);
+
+    // Injecting
     this.rootEl.insertAdjacentElement("afterbegin", this.formEl);
+  }
+
+  @SyncBinding
+  private submitHandler(e: Event) {
+    e.preventDefault();
+    console.log(this.titleInputEl.value);
   }
 }
 
