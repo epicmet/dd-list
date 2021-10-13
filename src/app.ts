af28436b034c2266e822d6acec495c1f3a2f974e
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-10-13 18:30:13 +0330

    extraced input values and validate them

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,37 @@
+// Validation
+interface ValidationObj {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function validate(confObj: ValidationObj) {
+  let isValid = true;
+
+  if (confObj.required) {
+    isValid = isValid && confObj.value.toString().trim().length !== 0;
+  }
+
+  if (confObj.minLength != null && typeof confObj.value === "string") {
+    isValid = isValid && confObj.value.length >= confObj.minLength;
+  }
+  if (confObj.maxLength != null && typeof confObj.value === "string") {
+    isValid = isValid && confObj.value.length <= confObj.maxLength;
+  }
+
+  if (confObj.min != null && typeof confObj.value === "number") {
+    isValid = isValid && confObj.value >= confObj.min;
+  }
+  if (confObj.max != null && typeof confObj.value === "number") {
+    isValid = isValid && confObj.value <= confObj.max;
+  }
+
+  return isValid;
+}
+
 // Decorators
 function SyncBinding(
   _1: any,
@@ -46,10 +80,39 @@ class ProjectInput {
     this.rootEl.insertAdjacentElement("afterbegin", this.formEl);
   }
 
+  private clearInputs() {
+    this.titleInputEl.value = "";
+    this.descriptionInputEl.value = "";
+    this.peopleInputEl.value = "";
+  }
+
+  private getFormInfo(): [string, string, number] | void {
+    const titleVal = this.titleInputEl.value;
+    const descVal = this.descriptionInputEl.value;
+    const peopleVal = this.peopleInputEl.value;
+
+    if (
+      validate({ value: titleVal, required: true }) &&
+      validate({ value: descVal, required: true, minLength: 5 }) &&
+      validate({ value: +peopleVal, required: true, min: 1, max: 5 })
+    ) {
+      return [titleVal, descVal, +peopleVal];
+    } else {
+      alert("Invalid input");
+      return;
+    }
+  }
+
   @SyncBinding
   private submitHandler(e: Event) {
     e.preventDefault();
-    console.log(this.titleInputEl.value);
+
+    const userInput = this.getFormInfo();
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      this.clearInputs();
+      console.log(title, desc, people);
+    }
   }
 }
 
