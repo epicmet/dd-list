c76ece22b2745b274a1f05d7ca154b20c06c061f
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-10-17 15:41:54 +0330

    made production config

---
+++ webpack.config.prod.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const CleanPlugin = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/app.ts",
+  output: {
+    filename: "app.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./index.html" }),
+    new CleanPlugin.CleanWebpackPlugin(),
+  ],
+};
