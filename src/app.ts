dbe00f58c2904ca98a8d513b1c20eb95807477dc
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-10-17 15:39:27 +0330

    loaded app.css file

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import ProjectInput from "./Components/input";
 import ProjectList from "./Components/list";
 
+import "../app.css";
+
 new ProjectInput();
 new ProjectList("active");
 new ProjectList("finished");
